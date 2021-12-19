ae42e46e56de94baf1b113b100a9d7280e20657f
Joe Benz <joebenz14@gmail.com>
2021-12-19 18:27:44 -0500

    initial structuring and text check

--- working3.R
+++ working3.R
@@ -1,34 +1,11 @@
-library(shiny)
+Carib_extent <- extent(c(xmin=-130,xmax=-17,ymin=-26,ymax=33))
+bbox1 <- bbox(Carib_extent)
 
-ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(bottom = 30, right = 10,
-                sliderInput("Year", "Year", 1995, 2021, value = 1995, step = 1, sep = "")
-  )
-)
+OM <- occ("Ophiothela mirabilis", from = c('gbif','bison','inat', 'ebird', 'vertnet'), has_coords=TRUE, limit=1000000, geometry=bbox1)
+OMdf <- occ2df(OM) %>%
+  mutate(simplename="Ophiothela mirabilis")
 
-server <- function(input, output, session) {
-  
-  sliderData <- reactive({
-    OMdf %>%
-      filter(Year <= input$Year)
-  })
-  
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%
-      fitBounds(min(OMdf$longitude), min(OMdf$latitude),
-                max(OMdf$longitude), max(OMdf$latitude))
-  })
-  
-  observe({
-    leafletProxy("map", data=sliderData()) %>%
-      clearMarkers() %>%
-      addTiles() %>%
-      setView(lng=-74.431642, lat=8.399157, zoom=3) %>%
-      addMarkers(lng = ~longitude, lat = ~latitude)
-  })
-}
+OMdf$Year <- format(as.Date(OMdf$date, format="%Y/%m/%d"),"%Y")  
 
-shinyApp(ui = ui, server = server)
+OMdf2 <- OMdf %>%
+  filter(Year!=1971)
